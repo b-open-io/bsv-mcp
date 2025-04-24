38262e5a072571455bf6fded721def6af00ace98
Satchmo <rohenaz@gmail.com>
2025-04-24 14:45:28 -0400

    bump version

--- index.ts
+++ index.ts
@@ -70,7 +70,7 @@ function initializePrivateKey(): PrivateKey | undefined {
 const privKey = initializePrivateKey();
 
 const server = new McpServer(
-	{ name: "Bitcoin SV", version: "0.0.30" },
+	{ name: "Bitcoin SV", version: "0.0.31" },
 	// {
 	// 	// Advertise only what you actually implement
 	// 	capabilities: {
